181877026647700e0e2766d8f2c46d34069ddd25
Andrii Pleskanko <andrii.pleskanko@playtech.com>
2025-10-21 20:37:42 +0300

    Implement Phong shading in shaders and use Facet Area Weighted normals

--- Model.js
+++ Model.js
@@ -1,114 +1,127 @@
+(function () {
+    // Constructor
+    function Model(options) {
+        options = options || {};
+        this.uSegments = options.uSegments || 40;
+        this.vSegments = options.vSegments || 40;
 
+        this.a = 1;
+        this.c = 1;
+        this.k = 1.1;
 
-function deg2rad(angle) {
-    return angle * Math.PI / 180;
-}
+        this.meshVertices = null;
+        this.meshNormals = null;
+        this.meshIndices = null;
+        this.vertexBuffer = null;
+        this.normalBuffer = null;
+        this.indexBuffer = null;
 
-
-function Vertex(p)
-{
-    this.p = p;
-    this.normal = [];
-    this.triangles = [];
-}
-
-function Triangle(v0, v1, v2)
-{
-    this.v0 = v0;
-    this.v1 = v1;
-    this.v2 = v2;
-    this.normal = [];
-    this.tangent = [];
-}
-
-// Constructor
-function Model(name) {
-    this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
-    this.iIndexBuffer = gl.createBuffer();
-    this.count = 0;
-
-    this.BufferData = function(vertices, indices) {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
-        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribVertex);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STREAM_DRAW);
-
-        this.count = indices.length;
+        this.createMeshData();
     }
 
-    this.Draw = function() {
+    Model.prototype.cassini = function (u, v) {
+        var a = this.a, c = this.c, k = this.k;
+        var r = Math.pow(Math.pow(Math.cos(2 * v), 2) + Math.pow(k, 4) - Math.pow(Math.sin(2 * v), 2), 0.25);
+        if (isNaN(r)) r = 0; // Додано перевірку, щоб уникнути NaN
+        var x = a * r * Math.cos(u);
+        var y = a * r * Math.sin(u);
+        var z = c * Math.sin(v);
+        return [x, y, z];
+    };
 
-        //gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-        gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
-    }
-}
+    Model.prototype.createMeshData = function () {
+        const uSegs = this.uSegments;
+        const vSegs = this.vSegments;
+        const uMin = -Math.PI, uMax = Math.PI;
+        const vMin = -Math.PI / 4, vMax = Math.PI / 4;
 
+        let vertices = [];
 
-function CreateSurfaceData(data)
-{
-    let vertices = [];
-    let triangles = [];
-
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 0, Math.cos(deg2rad(ang))] ));
-    }
-
-    for (let i=0, ang = 0; i<72; i++, ang+=5) {
-
-        let v0ind = vertices.length;
-        vertices.push( new Vertex( [Math.sin(deg2rad(ang)), 1, Math.cos(deg2rad(ang))] ));
-
-        // v0    v2 
-        //   o - o
-        //   | \ |
-        //   o - o
-        // v3     v1
-
-        if (i > 0)
-        {
-            let v1ind = v0ind - 72 -1;
-            let v2ind = v0ind - 1;
-            let v3ind = v0ind - 72
-
-            let trian = new Triangle(v0ind, v1ind, v2ind);
-            let trianInd = triangles.length;
-
-            triangles.push( trian );
-            vertices[v0ind].triangles.push(trianInd);
-            vertices[v1ind].triangles.push(trianInd);
-            vertices[v2ind].triangles.push(trianInd);
-
-            let trian2 = new Triangle(v0ind, v3ind, v1ind);
-            let trianInd2 = triangles.length;
-
-            triangles.push( trian2 );
-            vertices[v0ind].triangles.push(trianInd2);
-            vertices[v3ind].triangles.push(trianInd2);
-            vertices[v1ind].triangles.push(trianInd2);
-
+        for (let j = 0; j <= vSegs; ++j) {
+            let v = vMin + (vMax - vMin) * j / vSegs;
+            for (let i = 0; i <= uSegs; ++i) {
+                let u = uMin + (uMax - uMin) * i / uSegs;
+                let pt = this.cassini(u, v);
+                vertices.push(pt[0], pt[1], pt[2]);
+            }
         }
 
-    }
+        // Генеруємо індекси для трикутників
+        let indices = [];
+        for (let j = 0; j < vSegs; ++j) {
+            for (let i = 0; i < uSegs; ++i) {
+                let i0 = j * (uSegs + 1) + i;
+                let i1 = i0 + 1;
+                let i2 = i0 + (uSegs + 1);
+                let i3 = i2 + 1;
+                indices.push(i0, i1, i2);
+                indices.push(i1, i3, i2);
+            }
+        }
 
-    data.verticesF32 = new Float32Array(vertices.length*3);
-    for (let i=0, len=vertices.length; i<len; i++)
-    {
-        data.verticesF32[i*3 + 0] = vertices[i].p[0];
-        data.verticesF32[i*3 + 1] = vertices[i].p[1];
-        data.verticesF32[i*3 + 2] = vertices[i].p[2];
-    }
+        // Розрахунок нормалей (Facet Area Weighted)
+        let normals = new Array(vertices.length).fill(0);
 
-    data.indicesU16 = new Uint16Array(triangles.length*3);
-    for (let i=0, len=triangles.length; i<len; i++)
-    {
-        data.indicesU16[i*3 + 0] = triangles[i].v0;
-        data.indicesU16[i*3 + 1] = triangles[i].v1;
-        data.indicesU16[i*3 + 2] = triangles[i].v2;
-    }
+        // --- ЕТАП 1: Накопичення зважених нормалей ---
+        for (let t = 0; t < indices.length; t += 3) {
+            let i0 = indices[t] * 3, i1 = indices[t + 1] * 3, i2 = indices[t + 2] * 3;
 
-}
+            let v0 = [vertices[i0], vertices[i0 + 1], vertices[i0 + 2]];
+            let v1 = [vertices[i1], vertices[i1 + 1], vertices[i1 + 2]];
+            let v2 = [vertices[i2], vertices[i2 + 1], vertices[i2 + 2]];
+
+            let a = [v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2]];
+            let b = [v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2]];
+
+            // Векторний добуток (його довжина ВЖЕ зважена за площею)
+            let nx = a[1] * b[2] - a[2] * b[1];
+            let ny = a[2] * b[0] - a[0] * b[2];
+            let nz = a[0] * b[1] - a[1] * b[0];
+
+            // Додаємо цей вектор до кожної з трьох вершин
+            normals[i0] += nx;
+            normals[i0 + 1] += ny;
+            normals[i0 + 2] += nz;
+            normals[i1] += nx;
+            normals[i1 + 1] += ny;
+            normals[i1 + 2] += nz;
+            normals[i2] += nx;
+            normals[i2 + 1] += ny;
+            normals[i2 + 2] += nz;
+        }
+
+        for (let i = 0; i < normals.length; i += 3) {
+            let nx = normals[i], ny = normals[i + 1], nz = normals[i + 2];
+            let len = Math.hypot(nx, ny, nz);
+            if (len > 0.00001) {
+                normals[i] /= len;
+                normals[i + 1] /= len;
+                normals[i + 2] /= len;
+            }
+        }
+
+        this.meshVertices = new Float32Array(vertices);
+        this.meshNormals = new Float32Array(normals);
+        this.meshIndices = new Uint16Array(indices);
+    };
+
+    Model.prototype.bufferMeshData = function (gl) {
+        if (this.vertexBuffer) gl.deleteBuffer(this.vertexBuffer);
+        if (this.normalBuffer) gl.deleteBuffer(this.normalBuffer);
+        if (this.indexBuffer) gl.deleteBuffer(this.indexBuffer);
+
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, this.meshVertices, gl.STATIC_DRAW);
+
+        this.normalBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, this.meshNormals, gl.STATIC_DRAW);
+
+        this.indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.meshIndices, gl.STATIC_DRAW);
+    };
+
+    window.Model = Model;
+})();
--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 let gl;                         // The webgl context.
-let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let cassiniModel;
+let uSlider, vSlider, uVal, vVal;
 
+function deg2rad(angle) {
+    return angle * Math.PI / 180;
+}
 
 // Constructor
 function ShaderProgram(name, program) {
@@ -14,72 +18,159 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
+    this.iAttribNormal = -1;
     // Location of the uniform specifying a color for the primitive.
     this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
-
-    this.Use = function() {
+    this.iModelViewMatrix = -1;
+    this.iNormalMatrix = -1;
+    this.iLightPosition = -1;
+    this.Use = function () {
         gl.useProgram(this.prog);
     }
 }
 
+const vertexShaderSource = `
+    attribute vec3 vertex;
+    attribute vec3 normal;
+    uniform mat4 ModelViewProjectionMatrix;
+    uniform mat4 ModelViewMatrix;
+    uniform mat3 NormalMatrix;
+    varying vec3 vNormal;
+    varying vec3 vPosition;
+    void main(void) {
+        vec4 viewPos = ModelViewMatrix * vec4(vertex, 1.0);
+        vPosition = viewPos.xyz;
+        vNormal = normalize(NormalMatrix * normal);
+        gl_Position = ModelViewProjectionMatrix * vec4(vertex, 1.0);
+    }
+`;
+
+const fragmentShaderSource = `
+    precision mediump float;
+    uniform vec4 color;
+    uniform vec3 lightPosition;
+    varying vec3 vNormal;
+    varying vec3 vPosition;
+
+    void main(void) {
+        vec3 N = normalize(vNormal);
+        vec3 L = normalize(lightPosition - vPosition); // вектор до світла
+        vec3 V = normalize(-vPosition);                // до камери
+        vec3 R = reflect(-L, N);                       // відбитий промінь
+
+        // Освітлення Phong
+        vec3 ambient = 0.1 * color.rgb;
+        vec3 diffuse = max(dot(N, L), 0.0) * color.rgb;
+        vec3 specular = pow(max(dot(R, V), 0.0), 32.0) * vec3(1.0); // блиск
+
+        vec3 result = ambient + diffuse + specular;
+        gl_FragColor = vec4(result, 1.0);
+    }
+`;
+
+function animateLight(time) {
+    const radius = 5.0;
+    const lx = radius * Math.cos(time * 0.5);
+    const ly = 3.0;
+    const lz = radius * Math.sin(time * 0.5);
+    return [lx, ly, lz];
+}
 
 /* Draws a colored cube, along with a set of coordinate axes.
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
+    let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
     /* Get the view matrix from the SimpleRotator object.*/
-    let modelView = spaceball.getViewMatrix();
+    let modelViewFromBall = spaceball.getViewMatrix();
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+    let translateToPointZero = m4.translation(0, 0, -10);
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    let modelMatrix = m4.multiply(rotateToPointZero, modelViewFromBall);
+    let modelViewMatrix = m4.multiply(translateToPointZero, modelMatrix);
+    let modelViewProjection = m4.multiply(projection, modelViewMatrix);
+    let normalMatrix4 = m4.transpose(m4.inverse(modelViewMatrix));
+    let normalMatrix = [
+        normalMatrix4[0], normalMatrix4[1], normalMatrix4[2],
+        normalMatrix4[4], normalMatrix4[5], normalMatrix4[6],
+        normalMatrix4[8], normalMatrix4[9], normalMatrix4[10]
+    ];
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
-    /* Multiply the projection matrix times the modelview matrix to give the
-       combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
+    shProgram.Use();
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+    gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewMatrix);
+    gl.uniformMatrix3fv(shProgram.iNormalMatrix, false, normalMatrix);
+    gl.uniform4fv(shProgram.iColor, [0.2, 0.7, 1.0, 1.0]);
 
-    surface.Draw();
+    const time = performance.now() * 0.001;
+    const lightWorldPos = animateLight(time); // 1. Це позиція у СВІТІ
+
+    const lightViewPos = m4.transformPoint(translateToPointZero, lightWorldPos);
+
+    gl.uniform3fv(shProgram.iLightPosition, lightViewPos);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cassiniModel.vertexBuffer);
+    gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cassiniModel.normalBuffer);
+    gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iAttribNormal);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cassiniModel.indexBuffer);
+    gl.drawElements(gl.TRIANGLES, cassiniModel.meshIndices.length, gl.UNSIGNED_SHORT, 0);
 }
-
-
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-    shProgram = new ShaderProgram('Basic', prog);
+    shProgram = new ShaderProgram('Phong', prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
-
-    let data = {};
-    
-    CreateSurfaceData(data)
-
-    surface = new Model('Surface');
-    surface.BufferData(data.verticesF32, data.indicesU16);
+    shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
+    shProgram.iColor = gl.getUniformLocation(prog, "color");
+    shProgram.iLightPosition = gl.getUniformLocation(prog, "lightPosition");
 
     gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+
+    function regenerateModel() {
+        let uSegs = parseInt(uSlider.value);
+        let vSegs = parseInt(vSlider.value);
+        uVal.textContent = uSegs;
+        vVal.textContent = vSegs;
+        cassiniModel = new Model({uSegments: uSegs, vSegments: vSegs});
+        cassiniModel.bufferMeshData(gl);
+    }
+
+    uSlider = document.getElementById("u-slider");
+    vSlider = document.getElementById("v-slider");
+    uVal = document.getElementById("u-val");
+    vVal = document.getElementById("v-val");
+
+    uSlider.addEventListener("input", regenerateModel);
+    vSlider.addEventListener("input", regenerateModel);
+
+    regenerateModel();
 }
 
+function animate() {
+    draw();
+    requestAnimationFrame(animate);
+}
 
 /* Creates a program for use in the WebGL context gl, and returns the
  * identifier for that program.  If an error occurs while compiling or
@@ -90,24 +181,24 @@ function initGL() {
  * source code for the vertex shader and for the fragment shader.
  */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShader);
     gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    }
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -121,25 +212,21 @@ function init() {
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if ( ! gl ) {
+        if (!gl) {
             throw "Browser does not support WebGL";
         }
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not get a WebGL graphics context.</p>";
+    } catch (e) {
+        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not get a WebGL graphics context.</p>";
         return;
     }
     try {
-        initGL();  // initialize the WebGL graphics context
-    }
-    catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
-            "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
+        initGL();
+    } catch (e) {
+        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
         return;
     }
 
     spaceball = new TrackballRotator(canvas, draw, 0);
 
-    draw();
+    animate();
 }
